34837530f2f7cc11ef49f062a4d99c65f389eb7d
Sivert <sivertgh@stud.ntnu.no>
2023-01-08 00:47:34 +0100

    add rubik

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" style={{ backgroundColor: 'rgb(37, 39, 50)' }} id="home">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,5 @@ module.exports = {
     rtl: false,
     prefix: "",
     darkTheme: "dracula",
-  },
+  }
 }
