523393eb9ce7b1c94b001cefcbe4659747bb48af
Sivert <sivertgh@stud.ntnu.no>
2023-01-09 22:47:04 +0100

    added file names

--- config/constants.ts
+++ config/constants.ts
@@ -3,7 +3,7 @@ import { downloadLinkType } from './types'
 export const appName = 'Gryt' as const
 export const selfHostLink = 'https://docs.gryt.chat/docs/server/introduction/installation' as const
 export const downloadLinks: downloadLinkType = {
-    windows: '#',
-    macos: '#',
-    linux: '#',
+    windows: '#windows',
+    macos: '#macos',
+    linux: '#linux',
 } as const
