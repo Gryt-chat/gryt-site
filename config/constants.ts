3e8c204ebc6f55ba893bfeb0704a415ee22a77c1
Sivert <sivertgh@stud.ntnu.no>
2023-01-10 17:32:37 +0100

    Fixed macos incorrect

--- config/constants.ts
+++ config/constants.ts
@@ -3,7 +3,7 @@ import { downloadLinkType } from './types'
 export const appName = 'Gryt' as const
 export const selfHostLink = 'https://docs.gryt.chat/docs/server/introduction/installation' as const
 export const downloadLinks: downloadLinkType = {
-    windows: '#windows',
-    macos: '#macos',
-    linux: '#linux',
+    'windows': '#windows',
+    'mac os': '#macos',
+    'linux': '#linux',
 } as const
