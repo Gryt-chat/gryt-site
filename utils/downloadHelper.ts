8fec1b97c0db2e4455bc527f55bb62edde3435b4
Sivert <sivertgh@stud.ntnu.no>
2023-01-10 17:32:44 +0100

    Fixed download not working

--- utils/downloadHelper.ts
+++ utils/downloadHelper.ts
@@ -2,7 +2,9 @@ import JsFileDownloader from 'js-file-downloader';
 import { downloadLinks } from '../config/constants';
 
 export function download(os: string) {
-    const s = downloadLinks[os] ? downloadLinks[os] : ''
+    const s = downloadLinks[os.toLowerCase()] ? downloadLinks[os.toLowerCase()] : false
+    console.log(`Attempt to download for ${os}, is available: ${s}`);
+    
     if (s)
     new JsFileDownloader({url: s})
     .then(function () {
