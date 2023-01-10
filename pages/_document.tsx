acd36e214d189acff1638a5052f08094a5a82ac4
Sivert <sivertgh@stud.ntnu.no>
2023-01-10 19:09:54 +0100

    Updated favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,26 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+
         <meta name="title" content="Gryt" />
         <meta name="description" content={description} />
         <meta name="theme-color" content="#252732" />
