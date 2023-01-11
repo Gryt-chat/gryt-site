bf79f05a3233fcc036ef40ccbf6c354a7f8142b9
Sivert <sivertgh@stud.ntnu.no>
2023-01-11 20:30:10 +0100

    Updated self host link

--- config/constants.ts
+++ config/constants.ts
@@ -1,7 +1,7 @@
 import { downloadLinkType } from './types'
 
 export const appName = 'Gryt' as const
-export const selfHostLink = 'https://docs.gryt.chat/docs/server/introduction/installation' as const
+export const selfHostLink = 'https://docs.gryt.chat/docs/server/introduction/setup' as const
 export const downloadLinks: downloadLinkType = {
     'windows': '#windows',
     'mac os': '#macos',
