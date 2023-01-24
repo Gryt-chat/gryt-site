2cadfb7ec265195c03c95fff5c0046831e15b8c3
Sivert <sivertgh@stud.ntnu.no>
2023-01-24 22:55:27 +0100

    new theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   // daisyUI config (optional)
   daisyui: {
     styled: true,
-    themes: ['dracula'],
+    themes: [
+      {
+        grytApp: {
+          primary: "#948df5",
+          secondary: "#f5948d",
+          accent: "#8dbaf5",
+          neutral: "#191D24",
+          "base-100": "#2A303C",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
-    prefix: '',
+    prefix: "",
   },
 };
